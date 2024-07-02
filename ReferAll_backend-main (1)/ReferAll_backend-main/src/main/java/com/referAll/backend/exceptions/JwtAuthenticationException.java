d15debd5629e3a65e4a86f64925039831f6a4c6b
Terminator-006 <siwangaurish003@gmail.com>
2024-07-02 14:56:45 +0530

    Delete ReferAll_backend-main (1)/ReferAll_backend-main directory

--- ReferAll_backend-main (1)/ReferAll_backend-main/src/main/java/com/referAll/backend/exceptions/JwtAuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.referAll.backend.exceptions;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class JwtAuthenticationException extends AuthenticationException {
-    public JwtAuthenticationException(String message) {
-        super(message);
-    }
-}
